118ef10664c6181d6fb92ca4e0536301a47e2dca
javier echeverri <jecheverrid@gmail.com>
2020-05-23 11:06:56 +0200

    seed added to the db - ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'open-uri'
+
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+doc = open(url).read
+list = JSON.parse(doc)
+
+list["drinks"].each do |drink_hash|
+  ingredient_name = drink_hash["strIngredient1"]
+  new_ingredient = Ingredient.new(name: ingredient_name)
+  puts "Ingredient #{new_ingredient.name} saved!" if new_ingredient.save
+end
